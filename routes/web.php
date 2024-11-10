c4c404b9db365f853b34ace8532767a52b0c4f9e
fauzan <fauzanmuh313@gmail.com>
2024-11-10 08:44:32 +0700

    add redirects in toml

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', [App\Http\Controllers\LoginController::class, 'index'])->name('login.form');
 Route::post('/', [App\Http\Controllers\LoginController::class, 'login'])->name('login');
